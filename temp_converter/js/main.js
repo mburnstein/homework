b7103348d6e4de7db49371e82ab871ac453d632f
mburnstein <michael@burnstein.com>
2016-04-07 18:21:10 -0700

    Works now

--- temp_converter/js/main.js
+++ temp_converter/js/main.js
@@ -1,1 +1,26 @@
 
+//Wait for #fahrenheit_to_celsius click
+
+$('#fahrenheit_to_celsius').on('click', function(){
+
+
+//Grab string from #temperature and store in variable
+
+    var fahrenheit = $('#temperature').val();
+    
+
+//parseFloat convert to number
+
+    var intNumber=parseFloat(fahrenheit);
+
+//convert to celsius
+
+    var celsius=(intNumber-32)/1.8;
+
+//produce the result and store in variable
+
+    $('#result').html(celsius);
+//Display celsius variable html method
+
+})
+
